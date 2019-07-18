c66081e8cba5984a0eced6e05788e277ce09d142
anhuy1989 <nguyenanhuy1989@gmail.com>
2019-07-18 16:15:21 +0700

    Add KCO confirmation

--- src/module-vsf-kco/Controller/Order/Confirmation.php
+++ src/module-vsf-kco/Controller/Order/Confirmation.php
@@ -8,14 +8,16 @@ use Klarna\Ordermanagement\Api\ApiInterface;
 use Klarna\Ordermanagement\Model\Api\Ordermanagement;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Quote\Api\CartRepositoryInterface;
 use Magento\Quote\Model\QuoteIdMaskFactory;
 use Magento\Quote\Model\QuoteManagement;
+use Magento\Store\Model\ScopeInterface;
 use Magento\Store\Model\StoreManagerInterface;
 use Psr\Log\LoggerInterface;
 
 
-class Push extends Action
+class Confirmation extends Action
 {
 
     /**
@@ -59,6 +61,8 @@ class Push extends Action
      */
     private $quoteIdMaskFactory;
 
+    private $scopeConfig;
+
     /**
      * Push constructor.
      * @param Context $context
@@ -69,7 +73,6 @@ class Push extends Action
      * @param CartRepositoryInterface $cartRepository
      * @param Ordermanagement $orderManagement
      * @param StoreManagerInterface $storeManager
-     * @param QuoteIdMaskFactory $quoteIdMaskFactory
      */
 
     public function __construct(
@@ -81,7 +84,9 @@ class Push extends Action
         CartRepositoryInterface $cartRepository,
         Ordermanagement $orderManagement,
         StoreManagerInterface $storeManager,
-        QuoteIdMaskFactory $quoteIdMaskFactory
+        QuoteIdMaskFactory $quoteIdMaskFactory,
+        ScopeConfigInterface $scopeConfig
+
     ) {
         $this->logger = $logger;
         $this->klarnaOrderFactory = $klarnaOrderFactory;
@@ -91,6 +96,8 @@ class Push extends Action
         $this->orderManagement = $orderManagement;
         $this->storeManager = $storeManager;
         $this->quoteIdMaskFactory = $quoteIdMaskFactory;
+        $this->scopeConfig = $scopeConfig;
+
         parent::__construct(
             $context
         );
@@ -99,67 +106,52 @@ class Push extends Action
     public function execute()
     {
         $klarnaOrderId = $this->getRequest()->getParam('id');
-        $this->logger->info('Pussing Klarna Order Id: ' . $klarnaOrderId);
+        $this->logger->info('Confirmation Klarna Order Id: ' . $klarnaOrderId);
         $store = $this->storeManager->getStore();
         if (!$klarnaOrderId) {
-            echo 'Klarna Order ID is required';
+            $this->logger->info('Klarna Order ID is required');
             return;
         }
         $klarnaOrder = $this->klarnaOrderRepository->getByKlarnaOrderId($klarnaOrderId);
 
+        if ($klarnaOrder->getOrderId()) {
+            $this->logger->info('Error: Order already exists in Magento: ' . $klarnaOrder->getOrderId());
+            return;
+        }
         if ($klarnaOrder->getIsAcknowledged()) {
-            echo 'Error: Order ' . $klarnaOrderId . ' has been acknowledged';
+            $this->logger->info('Error: Order ' . $klarnaOrderId . ' has been acknowledged');
             return;
         }
 
         $this->orderManagement->resetForStore($store, ConfigHelper::KCO_METHOD_CODE);
         $placedKlarnaOrder = $this->orderManagement->getPlacedKlarnaOrder($klarnaOrderId);
-
         $maskedId = $placedKlarnaOrder->getDataByKey('merchant_reference2');
         $quoteIdMask = $this->quoteIdMaskFactory->create()->load($maskedId, 'masked_id');
         $quoteId = $quoteIdMask->getQuoteId();
         $quote = $this->cartRepository->get($quoteId);
-        if (!$quote->getId()) {
-            echo 'Quote is not existed in Magento';
-        }
-
-        if ($klarnaOrder->getOrderId()) {
-            $this->acknowledgeOrder($klarnaOrderId, $klarnaOrder->getOrderId(), $quoteId);
-            return;
-        } else {
+        $resultRedirect = $this->resultRedirectFactory->create();
+        if ($quote->getId()) {
             try {
                 $order = $this->quoteManagement->submit($quote);
-                $this->acknowledgeOrder($klarnaOrderId, $order->getId(), $quoteId);
-                echo 'Magento order created with ID ' . $order->getIncrementId();
-                return;
+                $orderId = $order->getId();
+                if ($orderId) {
+                    $klarnaOrder->setOrderId($orderId)->save();
+                }
+                $this->logger->info('Magento order created with ID ' . $order->getIncrementId());
+                $successUrl = $this->scopeConfig->getValue('klarna/vsf/successful_link', ScopeInterface::SCOPE_STORES, $store);
+                $resultRedirect->setUrl($successUrl.'?sid='.$klarnaOrderId);
+                return $resultRedirect;
+
             } catch (\Exception $exception) {
-                echo 'Create order error: ' . $exception->getMessage();
-                //Cancel Klarna Order if can not create order from merchant
+                $this->logger->critical('Create order error' . $exception->getMessage());
                 $this->orderManagement->cancel($klarnaOrderId);
-                return;
             }
         }
-        exit;
+        $failedUrl = $this->scopeConfig->getValue('klarna/vsf/failed_link', ScopeInterface::SCOPE_STORES, $store);
+        $resultRedirect->setUrl($failedUrl);
+        return $resultRedirect;
+
+
     }
 
-    private function acknowledgeOrder($klarnaOrderId, $orderId, $quoteId)
-    {
-        if ($klarnaOrderId && $orderId && $quoteId) {
-            try {
-                $klarnaOrder = $this->klarnaOrderRepository->getByKlarnaOrderId($klarnaOrderId);
-                $this->orderManagement->updateMerchantReferences($klarnaOrderId, $orderId, $quoteId);
-                $this->orderManagement->acknowledgeOrder($klarnaOrderId);
-                $klarnaOrder->setOrderId($orderId)
-                    ->setIsAcknowledged(true)
-                    ->save();
-                echo 'Sent ACK successfully with Klarna ID: ' . $klarnaOrderId;
-                return;
-            } catch (\Exception $exception) {
-                echo 'Send ACK error: ' . $exception->getMessage();
-            }
-        } else {
-            echo 'Something went wrong when sending ACK';
-        }
-        return;
-    }
 }
